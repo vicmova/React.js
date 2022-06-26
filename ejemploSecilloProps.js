cb1bf111c06190c88b0cf4db9ba817ae99c77591
Victor Moragues <30898324+vicmova@users.noreply.github.com>
2022-06-26 09:40:15 +0200

    con contador

--- ejemploSecilloProps.js
+++ ejemploSecilloProps.js
@@ -17,6 +17,37 @@ export default function Saludo(props){
 
 }
 
+//con contador
+
+import { useState } from "react";
+
+//recibe props definidos en App.js
+
+export default function Saludo(props){
+
+    const[contador, setContador] = useState(0);
+    
+    //vemos qué estamos recibiendo (qué hemos definido) desde App.js
+    console.log(props);
+
+    const { mensaje, nombre, code } = props;
+    return(
+
+        <div>
+
+        <button type="submit" onClick={() => (setContador(contador +1))}>Cuenta</button>
+        
+        <p><h2>Mostrar Componente: Saludo y prop nombre: {nombre} {code} </h2> </p>
+        
+        <p>{mensaje}</p>
+
+        <h3>Contador: {contador}</h3>
+        
+        </div>
+    );
+
+} 
+
 //////////////////////////////////////////////////////////
 //////////////fichero App.js//////////////////////////////
 //////////////////////////////////////////////////////////
